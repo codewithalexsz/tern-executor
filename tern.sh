a3f20ca91f2fe9e254c7f3034f736ebb77bed600
Alex Dot dev <55211588+codewithalexsz@users.noreply.github.com>
2024-12-20 08:04:43 +0100

    Update tern.sh

--- tern.sh
+++ tern.sh
@@ -10,7 +10,7 @@ run_with_delay() {
 run_with_delay rm -r executor-linux-v0.27.0.tar.gz
 
 # Download the new tar.gz file
-run_with_delay wget https://github.com/t3rn/executor-release/releases/download/v0.28.0/executor-linux-v0.28.0.tar.gz
+run_with_delay wget https://github.com/t3rn/executor-release/releases/download/v0.29.0/executor-linux-v0.29.0.tar.gz
 
 # Extract the tar.gz file
 run_with_delay tar -xvzf executor-linux-v0.28.0.tar.gz
@@ -19,13 +19,14 @@ run_with_delay tar -xvzf executor-linux-v0.28.0.tar.gz
 cd executor/executor/bin || exit
 
 # Set environment variables
+run_with_delay export EXECUTOR_MAX_L3_GAS_PRICE=10
 run_with_delay export NODE_ENV=testnet
 run_with_delay export LOG_LEVEL=debug
 run_with_delay export LOG_PRETTY=false
 run_with_delay export EXECUTOR_PROCESS_ORDERS=true
 run_with_delay export EXECUTOR_PROCESS_CLAIMS=true
 run_with_delay export EXECUTOR_MAX_L3_GAS_PRICE=50
-run_with_delay export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn'
+run_with_delay export  ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,optimism-sepolia,l1rn'
 run_with_delay export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
 
 # Ask the user for their private key
